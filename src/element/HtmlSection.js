5397c62ddc2658f18422c3599f33c96be76bbccb
Aaron <aaron4uk@gmail.com>
2018-10-11 05:39:12 -0500

    renamed file - body to section

---
+++ src/element/HtmlSection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+/**
+ * Returns components for the main content area
+ */
+const HtmlSection = () => {
+  return <section>section</section>
+}
+
+export default HtmlSection
